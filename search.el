508dfba623f4851bfa30beaaa35da12e18cab942
boyw165 <boyw165@gmail.com>
2015-01-29 17:10:54 +0800

    no message

---
+++ search.el
@@ -0,0 +1,46 @@
+;;; hl-anything.el --- Highlight symbols, selections, enclosing parens and more.
+;;
+;; Copyright (C) 2015
+;;
+;; Author: boyw165
+;; Version: 20150115.1500
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;; 
+;;
+;;; Code:
+
+;; GNU Library.
+
+(defgroup search nil
+  “Search.”)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Common ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(provide ‘search)
+;;; search.el ends here
